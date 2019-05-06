f1c9a031cacd6ebcb32500f421ab9bd618239267
Adrian <aacampbe@buffalo.edu>
2019-05-05 23:47:10 -0400

    Epic to the max.

--- src/theGame/Game.scala
+++ src/theGame/Game.scala
@@ -9,11 +9,38 @@ class Game {
   var Line: Double = -0.3
   //circle on circle collision (orc attack)
 
-  def detectCollision(player: Player, pla): Boolean = {
-    Math.pow(enemy.xLocation - player.xLocation, 2) + (player.xLocation - enemy.yLocation) <= Math.pow(player.radius + enemy.radius, 2)
-  }
+  def detectCollision(player: Player, player2: Player, boundary: Boundary): Boolean = {
+   if(player.location.x == player2.location.x && player.location.y == player2.location.y){
+     return false
+   }
+    val mObj = Physics.slope(player.location, player2.location)
+    val bObj = Physics.yIntercept(player.location, mObj)
 
-  // square on circle collision (melee/projectile hit boxes are a square)
+    val mBound = Physics.slope(boundary.start, boundary.end)
+    val bBound = Physics.yIntercept(boundary.start, mBound)
+    if (Physics.equalDoubles(mObj, mBound)){
+      return false
+    }
+
+    //    m1x + b1 = m2x + b2
+    //    m1x - m2x = b2 - b1
+    //    x(m1x - m2) = b2 - b1
+    //    x = (b2 - b1) / (m1x - m2)
+
+    val ix: Double = (bBound - bObj) / (mObj - mBound)
+    val iy: Double = ix * mObj + bObj
+    val iy_redundant: Double = ix * mBound + bBound
+    val objLeft = player.location.x.min(player2.location.x)
+    val objRight = player.location.x.max(player2.location.x)
+
+    val objUp = player.location.y.min(player2.location.y)
+    val objDown = player.location.y.max(player2.location.y)
+    val bLeft = boundary.start.x.min(boundary.end.x)
+    val bRight = boundary.start.x.max(boundary.end.x)
+    val bUp = boundary.start.y.min(boundary.end.y)
+    val bDown = boundary.start.y.max(boundary.end.y)
+    ((ix >= objLeft - Physics.EPSILON && ix <= objRight + Physics.EPSILON) && (iy >= objUp-Physics.EPSILON && iy <= objDown+Physics.EPSILON)) && ((ix >= bLeft -Physics.EPSILON&& ix <= bRight+Physics.EPSILON) && (iy >= bUp-Physics.EPSILON && iy <= bDown+Physics.EPSILON))
+  }
   def detectHit(enemy: Enemy, attack: Attack): Boolean = {
     val circleDistanceX = Math.abs(enemy.xLocation - attack.xLocation)
     val circleDistanceY = Math.abs(enemy.yLocation - attack.yLocation)
@@ -33,3 +60,5 @@ class Game {
     cornerDistance <= Math.pow(enemy.radius, 2)
   }
 }
+
+  // square on circle collision (melee/projectile hit boxes are a square)
--- src/theGame/Player.scala
+++ src/theGame/Player.scala
@@ -1,6 +1,31 @@
 package theGame
 
 class Player(location: PhysicsVector, velocity: PhysicsVector) extends PhysicalObject(location, velocity) {
+  var leftKeyHeld = false
+  var rightKeyHeld = false
+  var DownKeyHeld = false
+  def leftTouched(): Unit = {
+    this.leftKeyHeld = true
+  }
 
+  def rightTouched(): Unit = {
+    this.rightKeyHeld = true
+  }
+
+  def DownTouched(): Unit = {
+    this.DownKeyHeld = true
+  }
+
+  def leftReleased(): Unit = {
+    this.leftKeyHeld = false
+  }
+
+  def rightReleased(): Unit = {
+    this.rightKeyHeld = false
+  }
+
+  def DownReleased(): Unit = {
+    this.DownKeyHeld = false
+  }
 
 }
